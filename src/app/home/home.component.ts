3e44e6aa862a11c7dc6354815e8c30bc2bb4e50c
pefouk <jeremy.appriou@gmail.com>
2022-01-25 13:35:13 +0100

    Tailwind successfully set up and starting home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faAngry, faMap, faClock } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  faAngry = faAngry;
+  faMap = faMap;
+  faClock = faClock;
   constructor() { }
 
   ngOnInit(): void {
